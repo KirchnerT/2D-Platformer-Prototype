18c4c284263ca63c88e80e3b099d9b82429afb8b
Tyler Kirchner <tylerakirchner00@gmail.com>
2023-10-29 17:18:30 -0500

    Adding enemy and player stats

---
+++ 2D-Platformer-Prototype/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private float _health;
+    private float _stamina;
+    private float _basicCurrency;
+
+    private float startingHealth;
+    private float startingStamina;
+
+    private float staminaRechargeRate;
+    private float healthRechargeRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
